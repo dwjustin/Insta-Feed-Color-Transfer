86d7ed0e2223ac914334b4c33ee6fc7b1ef9f679
jimin <jiminc777@gmail.com>
2023-04-03 12:01:14 +0000

    input id and password

--- prototype_first.py
+++ prototype_first.py
@@ -9,6 +9,8 @@ TODO:
 1.     
 """
 
+# 123
+
 
 def insta_crawling(ID, PW, headless=False):
     cl = Client()
@@ -25,53 +27,57 @@ def insta_crawling(ID, PW, headless=False):
 
 st.title('AI color grader')
 st.subheader('Find the filter that best fits your Instagram feed!')
-uploaded_files = st.file_uploader(label="Choose image(s)...",
-                                  type=['jpeg', 'png', 'jpg', 'heic'],
-                                  label_visibility='visible',
-                                  accept_multiple_files=True)
+# uploaded_files = st.file_uploader(label="Choose image(s)...",
+#                                   type=['jpeg', 'png', 'jpg', 'heic'],
+#                                   label_visibility='visible',
+#                                   accept_multiple_files=True)
 
 
-crawled = []
-# Check if the user has uploaded any files
-if uploaded_files or crawled:
-    # Create an empty list to store the images
-    images = []
+# crawled = []
+# # Check if the user has uploaded any files
+# if uploaded_files or crawled:
+#     # Create an empty list to store the images
+#     images = []
 
-    # Loop through each uploaded file and append the opened image to the list
-    for file in uploaded_files:
-        image = Image.open(file)
-        images.append(image)
+#     # Loop through each uploaded file and append the opened image to the list
+#     for file in uploaded_files:
+#         image = Image.open(file)
+#         images.append(image)
 
-    # Calculate the number of rows and columns needed to display the images in a 3x3 grid
-    num_images = len(images)
-    num_rows = (num_images + 2) // 3
-    num_cols = min(num_images, 3)
+#     # Calculate the number of rows and columns needed to display the images in a 3x3 grid
+#     num_images = len(images)
+#     num_rows = (num_images + 2) // 3
+#     num_cols = min(num_images, 3)
 
-    # Set the desired width and height of the images in the grid
-    image_width = 200
+#     # Set the desired width and height of the images in the grid
+#     image_width = 200
 
-    # Loop through each row and column to display the images in a grid
-    for i in range(num_rows):
-        cols = st.columns(num_cols)
-        for j in range(num_cols):
-            index = i * 3 + j
-            if index < num_images:
-                cols[j].image(
-                    images[index],
-                    caption=f"{uploaded_files[index].name}",
-                    width=image_width)
-    # center_button = st.container()
-    # with center_button:
-    #     st.button("Process Images", text_align='center')
-    if st.button("Process Images!"):
-        st.write("Images are processed")
-
-
-else:
-    # If no files were uploaded, display a message
-    st.write("Please upload one or more image files.")
+#     # Loop through each row and column to display the images in a grid
+#     for i in range(num_rows):
+#         cols = st.columns(num_cols)
+#         for j in range(num_cols):
+#             index = i * 3 + j
+#             if index < num_images:
+#                 cols[j].image(
+#                     images[index],
+#                     caption=f"{uploaded_files[index].name}",
+#                     width=image_width)
+#     # center_button = st.container()
+#     # with center_button:
+#     #     st.button("Process Images", text_align='center')
+#     if st.button("Process Images!"):
+#         st.write("Images are processed")
 
 
+# else:
+#     # If no files were uploaded, display a message
+#     st.write("Please upload one or more image files.")
+
+insta_id = st.text_input("Put your Instagram ID here!")
+insta_pwd = st.text_input('Put your Instagram password here!')
 # Instagram crawling button
 if st.button("Crawling Instagram"):
-    insta_crawling("leessunj", "Ilsj08282!")
+    insta_crawling(insta_id, insta_pwd)
+
+#id = "leessunj"
+#pwd = "Ilsj08282!"
